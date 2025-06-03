1caf98ead09d25b07d109e28e027b416ee745a32
yarraguntababu <yarraguntababu954@gmail.com>
2025-06-03 11:47:20 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,7 +60,7 @@ document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
             document.title = "Portfolio | Babu";
-            $("#favicon").attr("href", "assets/images/favicon.png");
+            $("#favicon").attr("href", "assets/images/avt1.png");
         }
         else {
             document.title = "Come Back To Portfolio";
